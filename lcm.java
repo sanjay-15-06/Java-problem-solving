797eabfc6f6f8b3c9716fde2e08da574762ffe47
Sanjay s <sanjaysuresh1506@gmail.com>
2026-07-12 23:20:36 +0530

    LCM
     The LCM is the smallest positive number that is divisible by both numbers.

---
+++ lcm.java
@@ -0,0 +1,26 @@
+package java_tasks;
+
+import java.util.Scanner;
+
+class lcm {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int max = (a > b) ? a : b;
+
+        while (true) {
+
+            if (max % a == 0 && max % b == 0) {
+                System.out.println(max);
+                break;
+            }
+
+            max++;
+        sc.close();
+        }
+    }
+}
