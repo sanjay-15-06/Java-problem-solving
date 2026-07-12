8d221f837723a37a5b328000798fda321e22134e
Sanjay s <sanjaysuresh1506@gmail.com>
2026-07-12 23:21:18 +0530

    Sum of digits
     Add all the digits of the number.

---
+++ sum_of_digits.java
@@ -0,0 +1,24 @@
+package java_tasks;
+
+import java.util.Scanner;
+
+class sum_of_digits {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        while (num > 0) {
+
+            int rem = num % 10;
+            sum = sum + rem;
+            num = num / 10;
+        }
+
+        System.out.println(sum);
+    sc.close();
+    }
+}
